08d3a99b20fbf35b6d34ad4fc7a5cb8f16c35331
shikhark <conveytoshikhar@gmail.com>
2020-03-28 17:35:38 +0800

    Client home coming up

--- screens/ClientWelcomeScreen.js
+++ screens/ClientWelcomeScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text} from 'react-native'
-import { } from '../Components'
-import { dimens, colors } from '../constants'
+import { View, StyleSheet, Text, TouchableOpacity} from 'react-native'
+import { Heading, Card, Icon } from '../Components'
+import { dimens, colors, iconNames, customFonts } from '../constants'
 import { commonStyling } from '../common' 
 import {PropTypes} from 'prop-types'
 
@@ -14,7 +14,17 @@ class ClientWelcomeScreen extends Component {
   }
   render() {
     const {
-      mainContainer
+      mainContainer,
+      headingStyle,
+      personContainer,
+      upperCardContainer,
+      innerProfileCardContainer,
+      denominationContainer,
+      denomination,
+      denominationSubHeading,
+      transactionsMadeContainer,
+      innerProfileLowerCardContainer,
+      chairitesHelpedContainer
     } = styles
 
     const {
@@ -22,7 +32,34 @@ class ClientWelcomeScreen extends Component {
     } = this.props
     return (
       <View style={mainContainer}>
-        <Text> Hello from {this.state.name} </Text>
+        <Heading headingStyle={headingStyle} title='Welcome' heading/>
+        <View style={personContainer}>  
+          <Icon nameAndroid={iconNames.personAndroid} nameIOS={iconNames.personIOS} onPress={null} color={colors.colorPrimary}/>
+        </View>
+
+        <View style={upperCardContainer}>
+          <Card width='90%' height={180} elevation={5}>
+            <View style={innerProfileCardContainer}>
+
+                <View style={denominationContainer}>
+                  <Text style={denomination}>HKD 123.30</Text>
+                  <Text style={denominationSubHeading}>Total Avaiable Charitable Amount</Text>
+                </View>
+
+                <View style={innerProfileLowerCardContainer}>
+                  <View style={transactionsMadeContainer}>
+                    <Text style={denomination}>13</Text>
+                    <Text style={denominationSubHeading}>Monthy Transactions</Text>
+                  </View>
+                  <View style={chairitesHelpedContainer}>
+                    <Text style={denomination}>13</Text>
+                    <Text style={denominationSubHeading}>Charities Helped</Text>
+                  </View>
+                </View>
+            </View>
+
+          </Card>
+        </View>
       </View>
     );
   }
@@ -31,9 +68,74 @@ class ClientWelcomeScreen extends Component {
 const styles = StyleSheet.create({
   mainContainer: {
     ...commonStyling.mainContainer,
+  },
+  personContainer: {
+    position: 'absolute',
+    top: 50,
+    right: dimens.screenHorizontalMargin,
+    height: 50,
+    width: 50,
+    borderRadius: 40,
+    borderColor: colors.colorPrimary,
+    borderWidth: 2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  headingStyle: {
+    color: colors.colorPrimary,
+    marginTop: 55,
+    marginHorizontal: dimens.screenHorizontalMargin
+  },
+  upperCardContainer: {
+    width: '100%',
+    marginTop: 30,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  innerProfileCardContainer: {
+    padding: dimens.screenHorizontalMargin,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  denominationContainer: {
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    borderBottomColor: colors.grayTransluscent,
+    borderBottomWidth: 0.5,
+    paddingBottom: 12,
+    width: '100%'
+  },
+  innerProfileLowerCardContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  denomination: {
+    color: colors.submitGreen,
+    fontSize: 20,
+    fontFamily: customFonts.bold
+  },
+  denominationSubHeading: {
+    marginTop: 4,
+    fontSize: 13,
+    fontFamily: customFonts.regular,
+    color: colors.black
+  },
+  transactionsMadeContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRightColor: colors.grayTransluscent,
+    borderRightWidth: 0.5,
+    padding: 20,
+    width: '50%'
+  },
+  chairitesHelpedContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    width: '50%'
   }
+
 })
 
 ClientWelcomeScreen.navigationOptions = {
