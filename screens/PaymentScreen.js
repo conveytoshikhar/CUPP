22ab6e239a7110471ad26e89cf02d2f87f94a22f
Abhimanyue Singh Tanwar <singhabhimanyu186@gmail.com>
2020-04-25 13:12:15 +0530

    payment screen init

--- screens/PaymentScreen.js
+++ screens/PaymentScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text} from 'react-native'
-import { } from '../Components'
-import { dimens, colors } from '../constants'
+import { Heading} from '../Components'
+import { dimens, colors, customFonts } from '../constants'
 import { commonStyling } from '../common' 
 import {PropTypes} from 'prop-types'
 
@@ -9,20 +9,46 @@ class PaymentScreen extends Component {
   constructor(props){
     super(props)
     this.state = {
-      navigation: props.navigation
+      navigation: props.navigation,
+      item: props.navigation.getParam('course')
     }
   }
   render() {
     const {
-      mainContainer
+      mainContainer,
+      headingStyle,
+      priceDetailsContainer,
+      priceDetail,
+      priceType,
+      priceValue
     } = styles
 
     const {
       navigation
     } = this.props
+
+    const {
+      item
+    } = this.state
     return (
       <View style={mainContainer}>
-        <Text> Hello from Payment Screen</Text>
+        <View>
+        <Heading headingStyle={headingStyle} title= "Make Payment" />
+        </View>
+        <View style = {priceDetailsContainer}>
+          <View style={priceDetail}>
+          <Text style= {priceType}>Price</Text>
+          <Text style={priceValue}>{item.price}</Text>
+          </View>
+          <View style={priceDetail}>
+            <Text style={priceType}>Your Credit Score</Text>
+            <Text style={priceValue}>{item.price}</Text>
+          </View>
+          <View style={priceDetail}>
+            <Text style={priceType}>Service Charge</Text>
+          </View>
+        </View>
+        
       </View>
     );
   }
@@ -33,11 +59,33 @@ const styles = StyleSheet.create({
     ...commonStyling.mainContainer,
     alignItems: 'center',
     justifyContent: 'center'
-  }
+  },
+  headingStyle: {
+    fontSize: 25,
+    color: colors.colorPrimary,
+  },
+  priceDetailsContainer: {
+    marginTop: 20,
+    width: '100%',
+    flexDirection:'column',
+    justifyContent: 'center'
+  },
+  priceDetail: {
+    alignItems:'center'
+    },
+    priceType: {
+      fontFamily:customFonts.semiBold, 
+      fontSize:15
+    },
+    priceValue:{color:colors.colorPrimary, 
+      fontFamily:customFonts.medium, 
+      fontSize:20,
+      padding: 8
+    }
 })
 
 PaymentScreen.navigationOptions = {
-  title: 'PaymentScreen'
+  title: ''
 }
 
 PaymentScreen.propTypes = {
